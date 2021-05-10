01cf1334875c584fdf772dd4ed78a2e468338173
tnarravala <tnarravala@hawk.iit.edu>
2021-05-09 20:38:04 -0500

    may_9-updates

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
 from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 import dash_daq as daq
