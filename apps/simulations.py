67df54fc41d3a3ff86b0deb89c8a7112b2079608
tnarravala <tnarravala@hawk.iit.edu>
2021-05-10 14:20:15 -0500

    added new page

---
+++ apps/simulations.py
@@ -0,0 +1,318 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 10 13:33:13 2021
+
+@author: thejeswarreddynarravala
+"""
+
+import os
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
+import dash_daq as daq
+import plotly.express as px
+import pandas as pd
+import plotly.graph_objects as go
+
+from app import app
+
+cases = pd.read_csv("indian_cases_confirmed_cases.csv")
+deaths = pd.read_csv("indian_cases_confirmed_deaths.csv")
+imp_st = pd.read_csv('cases_deaths_india.csv')
+imp_st = imp_st.sort_values('date')
+
+state_dic = {'ap':'Andhra Pradesh',
+             'dl':'Delhi',
+             'mp':'Madhya Pradesh',
+             'kl':'Kerala',
+             'up':'Uttar Pradesh',
+             'mh':'Maharastra',
+             'br':'Bihar',
+             'wb':'West Bengal',
+             'tn':'Tamil Nadu',
+             'rj':'Rajesthan',
+             'ka':'Karnataka',
+             'gj':'Gujarat',
+             'or':'Odisha',
+             'tg':'Telangana',
+             'jh':'Jharkhand',
+             'as':'Assam',
+             'pb':'Punjab',
+             'ct':'Chattisgarh',
+             'hr':'Haryana',
+             'jk':'Jammu and Kashmir',
+             'ut':'Uttarakhand',
+             'hp':'Himachal Pradesh',
+             'tr':'Tripura',
+             'ml':'Meghalaya',
+             'mn':'Manipur',
+             'nl':'Nagaland',
+             'ga':'Goa',
+             'ar':'Arunachal Pradesh',
+             'py':'Puducherry',
+             'mz':'Mizoram',
+             'ch':'Chandigarh',
+             'sk':'Sikkim',
+             'dn_dd':'Daman and Diu',
+             'an':'Andaman and Nicobar',
+             'ld':'Ladakh',
+             'la':'Lakshdweep'}
+total_cases = cases.set_index('state')
+total_state_cases = total_cases.iloc[:,-1:]
+total_cases = total_state_cases.sum()
+
+total_deaths = deaths.set_index('state')
+total_state_deaths = total_deaths.iloc[:,-1:]
+total_deaths= total_state_deaths.sum()
+
+date_range = ["2021-02-10 18:36:37.3129", "2021-05-07 05:23:22.6871"]
+
+def plot_cases(state,ca):
+    sim_data = pd.read_csv(f'fitting_2021-05-03/{state}/sim.csv')
+    sim_data = sim_data[sim_data['series'] == 'G'].T
+    sim_data = sim_data[1:].reset_index()
+    sim_data.columns = ['date','G']
+    sim_data['date'] = pd.to_datetime(sim_data['date'])
+    dates =  sim_data['date']
+    if ca == False:
+        st = cases.set_index('state')
+        col1 = st['2020-01-30']
+        st = st.diff(axis = 1)
+        st['2020-01-30'] = col1
+        st = st.reset_index()
+        st = (st[st['state'] == state].T)
+        sim_data = sim_data['G'].diff()
+        sim_data[0] = 0
+        sim_data = sim_data.to_frame()
+        sim_data['date'] = dates
+        sim_data.columns = ['G','date']
+    else:
+        st = (cases[cases['state'] == state].T)
+
+    st = st[1:].reset_index()
+    st.columns = ['date','cases']
+    st['date'] = pd.to_datetime(st['date'])
+    st = st[st['date'] > '2021-01-31']
+    st_name = u'Cases in {}'.format(state_dic[state])
+    
+    fig = go.Figure()
+    fig.add_trace(go.Bar(x=st['date'],y = st['cases'],name="Actual G"))
+    fig.add_trace(go.Scatter(x=sim_data['date'],y = sim_data['G'],name="G"))
+    #fig = go.Figure()
+    #fig.add_trace(go.Scatter(x=st['date'],y=st['cases'],mode= 'markers',name='Cases'))
+    #fig.add_trace(go.Scatter(x=sim_data['date'],y=sim_data['infections'],mode= 'markers',name='I'))
+    #fig = make_subplots(rows = 6, cols =6, start_cell = "top-left")
+    #fig.add_trace(go.Scatter(x=st['date'],y=st['cases'],mode= 'markers'))
+    #fig = px.scatter(st, x='date', y='cases')
+    #fig = go.Figure()
+    #fig.add_trace(go.scatter(x=sim_data['date'],y=sim_data['infections'],mode ="lines",name="infections"))
+    #fig.add_trace(go.scatter(x=st['date'],y=st['cases'],mode ="lines"))
+    #fig.add_trace()
+    fig.update_layout(
+    autosize=True,
+    #title = st_name,
+    margin = dict(l=40, r=40, t=10, b=40 ),
+    width=500,
+    height=400,
+    yaxis = dict(
+       #range = [0,100] ,
+       #rangemode="tozero",
+        autorange=True,
+        title_text='Cases',
+        titlefont=dict(size=10),
+    ),
+    xaxis=dict(
+        title_text = "date",
+        autorange=True,
+        #range=date_range,
+        rangeslider=dict(
+            autorange=True,
+            range=date_range
+        ),
+        type="date"
+    ),
+    )
+
+    return fig
+
+def plot_deaths(state,ca):
+    sim_data = pd.read_csv(f'fitting_2021-05-03/{state}/sim.csv')
+    sim_data = sim_data[sim_data['series'] == 'D'].T
+    sim_data = sim_data[1:].reset_index()
+    sim_data.columns = ['date','D']
+    sim_data['date'] = pd.to_datetime(sim_data['date'])
+    dates =  sim_data['date']
+    if ca == False:
+        st = deaths.set_index('state')
+        col1 = st['2020-01-30']
+        st = st.diff(axis = 1)
+        st['2020-01-30'] = col1
+        st = st.reset_index()
+        st = (st[st['state'] == state].T)
+        sim_data = sim_data['D'].diff()
+        sim_data[0] = 0
+        sim_data = sim_data.to_frame()
+        sim_data['date'] = dates
+        sim_data.columns = ['D','date']
+    else:
+        st = (deaths[deaths['state'] == state].T)
+    st = st[1:].reset_index()
+    st.columns = ['date','deaths']
+    st['date'] = pd.to_datetime(st['date'])
+    st = st[st['date'] > '2021-01-31']
+    #fig = go.Figure()
+    #fig.add_trace(go.Scatter(x=st['date'],y=st['deaths'],mode= 'markers',name=f'{state_dic[state]}'))
+    st_name = u'Deaths in {}'.format(state_dic[state])
+    #fig = px.bar(st, x='date', y='deaths')
+    fig = go.Figure()
+    fig.add_trace(go.Bar(x=st['date'],y = st['deaths'],name="Actual D"))
+    fig.add_trace(go.Scatter(x=sim_data['date'],y = sim_data['D'],name="D"))
+    fig.update_layout(
+    autosize=True,
+    #title =  st_name,
+
+    margin = dict(l=40, r=40, t=10, b=40 ),
+    width=500,
+    height=400,
+    yaxis = dict(
+         #range = [0,100] ,
+         #rangemode="tozero",
+        autorange=True,
+        title_text='deaths',
+        titlefont=dict(size=10),
+    ),
+    xaxis=dict(
+        title_text = "date",
+        autorange=True,
+        range=date_range,
+        rangeslider=dict(
+            autorange=True,
+            range=date_range
+        ),
+        type="date"
+    ),
+    )
+
+    return fig
+
+body = dbc.Container([ 
+
+dbc.Row(
+        [
+    dcc.Dropdown(
+        id='sim_st',
+        options=[
+            {'label':'Andaman and Nicobar','value':'an'},
+            {'label': 'Andhra Pradesh', 'value': 'ap'},
+            {'label':'Arunachal Pradesh','value':'ar'},
+            {'label':'Assam','value':'as'},
+            {'label':'Bihar','value':'br'},
+            {'label':'Chandigarh','value':'ch'},
+            {'label':'Chattisgarh','value':'ct'},
+            {'label':'Daman and Diu','value':'dn_dd'},
+            {'label':'Delhi','value':'dl'},
+            {'label':'Goa','value':'ga'},
+            {'label':'Gujarat','value':'gj'},
+            {'label':'Haryana','value':'hr'},
+            {'label':'Himachal Pradesh','value':'hp'},
+            {'label':'Jammu and Kashmir','value':'jk'},
+            {'label':'Jharkhand','value':'jh'},
+            {'label':'Karnataka','value':'ka'},
+            {'label': 'Kerala', 'value': 'kl'},
+            {'label':'Ladakh','value':'ld'},
+            {'label':'Lakshdweep','value':'la'},
+            {'label': 'Madhya Pradesh', 'value': 'mp'},
+            {'label':'Maharastra','value':'mh'},
+            {'label':'Manipur','value':'mn'},
+            {'label':'Meghalaya','value':'ml'},
+            {'label':'Mizoram','value':'mz'},
+            {'label':'Nagaland','value':'nl'},
+            {'label':'Odisha','value':'or'},
+            {'label':'Puducherry','value':'py'},
+            {'label':'Punjab','value':'pb'},
+            {'label':'Rajesthan','value':'rj'},
+            {'label':'Sikkim','value':'sk'},
+            {'label':'Tamil Nadu','value':'tn'},
+            {'label':'Telangana','value':'tg'},
+            {'label':'Tripura','value':'tr'},
+            {'label':'Uttarakhand','value':'ut'},
+            {'label':'Uttar Pradesh','value':'up'},
+            {'label':'West Bengal','value':'wb'},
+ 
+        ],
+        value='dl',style = {'color':'black','width':'50%','display': 'inline-block','margin-left':'0.8%'}
+    ),
+            ]
+        ),
+      dbc.Row(
+        [html.Br()]),     
+    dbc.Row([
+        dbc.Col([html.H3(id = "sim_tc", style = {'display': 'inline-block'}),
+                 html.Br(),
+                 html.P("Cummulative",style = {'display': 'inline-block'}),
+                 daq.BooleanSwitch(
+                id='sim_cum-c',
+                on=False,
+                style = {'display': 'inline-block','size':'20%'}
+                        ),
+                               html.Br(),
+               html.P(id = "sim_title1", style = {'color':'green','display': 'inline-block'}),dcc.Graph(id='sim_fig',figure = plot_cases('dl',True))] ),
+        dbc.Col([
+            html.H3(id = "sim_td", style = {'display': 'inline-block'}),
+            html.Br(),
+                 html.P("Cummulative",style = {'display': 'inline-block'}),
+                 daq.BooleanSwitch(
+                id='sim_cum-d',
+                on=False,
+                style = {'display': 'inline-block','size':'20%'}
+                        ),
+                               html.Br(),
+            html.P(id = "sim_title2", style = {'color':'red','display': 'inline-block'}),
+            dcc.Graph(id='sim_fig2',figure = plot_deaths('dl',True))
+            
+            ])
+        ,]),                                                                               
+    
+
+],style={"height": "100vh"}
+
+)
+
+#app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SUPERHERO])
+server = app.server
+layout = html.Div([body])
+
+@app.callback(
+    Output('sim_fig', 'figure'),
+    Input('sim_st', 'value'),
+    Input('sim_cum-c','on'))
+def update_figure_sim(st,ca):
+    fig1 = plot_cases(st,ca)
+    fig1.update_layout(transition_duration=500)
+    return fig1
+
+@app.callback(
+    Output('sim_fig2', 'figure'),
+    Input('sim_st', 'value'),
+    Input('sim_cum-d','on'))
+def update_figure_sim2(st,ca):
+    fig2 = plot_deaths(st,ca)
+    fig2.update_layout(transition_duration=500)
+    return fig2
+
+@app.callback(
+    Output('sim_title1','children'),
+    Input('sim_st','value')
+    )
+def update_output_divsim1(st):
+    return u'Cases in {}'.format(state_dic[st]) 
+
+@app.callback(
+    Output('sim_title2','children'),
+    Input('sim_st','value')
+    )
+def update_output_divsim2(st):
+    return u'Deaths in {}'.format(state_dic[st]) 
