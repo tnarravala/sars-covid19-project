f3168d1dbc7c1a821124b4122f25ad7e1f86748d
tnarravala <tnarravala@hawk.iit.edu>
2021-05-10 14:27:54 -0500

    added new page

--- app.py
+++ app.py
@@ -14,6 +14,5 @@ import dash_bootstrap_components as dbc
 external_stylesheets = [dbc.themes.LUX]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
 server = app.server
 app.config.suppress_callback_exceptions = True
