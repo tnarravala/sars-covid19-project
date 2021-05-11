c8b2b112c5f6ce58a2ca70fcf4a984fda04d3c2e
tnarravala <tnarravala@hawk.iit.edu>
2021-05-11 13:16:56 -0500

    updated grided view with darker color

--- apps/home.py
+++ apps/home.py
@@ -300,7 +300,7 @@ def plot_all_states(state):
     #fig.add_trace(go.Scatter(x=sim_data['date'],y = sim_data['G']))
     #fig = px.bar(ct, x='date', y='confirmed_cases')
     fig.update_traces(marker_color='rgb(158,202,225)', marker_line_color='rgb(8,48,107)',
-                  marker_line_width=1.5, opacity=0.6)
+                   opacity=0.3)
     fig.update_layout(
     autosize=True,
     title = state_dic[state],
